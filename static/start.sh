94b0fb102efcdabba44f514637900f66d62d3599
GoodBoy119lab <sandinogoodboy@gmail.com>
2025-07-07 03:42:08 +0200

    Atualizado start.sh com comando de inicialização correto

--- static/start.sh
+++ static/start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# Inicia o servidor FastAPI usando uvicorn apontando para app/main.py
-uvicorn app.main:app --host 0.0.0.0 --port 10000
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-10000}
